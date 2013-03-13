5169f887153450831820125b3220c7395d2b698d
David Joos <david.joos@gmail.com>
2013-03-13 18:12:11 +0000

    metadata attribute changes

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,18 @@
+name             "gpac"
 maintainer       "Escape Studios"
 maintainer_email "dev@escapestudios.com"
 license          "MIT"
 description      "Installs/Configures gpac"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.4"
+version          "0.0.5"
 
-supports "ubuntu"
-supports "debian"
+%w{ debian ubuntu }.each do |os|
+supports os
+end
 
 depends "build-essential"
 depends "subversion"
+
+recipe "gpac", "Installs gpac."
+recipe "gpac::package", "Installs gpac using packages."
+recipe "gpac::source", "Installs gpac from source."
